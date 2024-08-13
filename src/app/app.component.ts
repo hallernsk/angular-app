b66b673596d7cd4ec5a8fb08e69c893abbe7af15
hallernsk <avladim@mail.ru>
2024-08-13 15:57:24 +0700

    changed component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +11,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-app';
+
+  // Новые свойства
+  message = 'Angular — это здорово!';
+  isSpecial = false;
+
+  // Метод для обработки клика
+  sayHello() {
+    this.message = 'Вы нажали на кнопку!';
+  }
+
+  toggleSpecial() {
+    this.isSpecial = !this.isSpecial;
+  }
 }
