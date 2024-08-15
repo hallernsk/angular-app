37eb1ab518d1db4cf2d33af18bb0bfb96346cba3
hallernsk <avladim@mail.ru>
2024-08-15 11:50:24 +0700

    added new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+// import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-card',
+    standalone: true,
+    templateUrl: './card.component.html',
+    styleUrl: './card.component.css'
+})
+export class CardComponent {
+    title = 'card-component';
+
+}
